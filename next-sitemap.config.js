a68c2eee62fdb40da1a8e57e1f0819697d1790e3
Kayhan Ayas <kyhnayas@gmail.com>
2025-07-26 04:16:48 +0300

    sitemap düzeltme

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+// next-sitemap.config.js
+
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://nicheprojectagency.com', // Sitenizin tam adresi
+  generateRobotsTxt: true, // Opsiyonel: robots.txt dosyasını da otomatik oluşturur
+  // Diğer gelişmiş ayarlar için dokümantasyonuna bakabilirsiniz.
+};
