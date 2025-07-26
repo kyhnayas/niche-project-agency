37b9893f9603536ea7e257a4d123eabcb5c1d475
Kayhan Ayas <kyhnayas@gmail.com>
2025-07-26 03:20:24 +0300

    demo sitemap

---
+++ app/robots.ts
@@ -0,0 +1,12 @@
+import { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: '/admin/', // Örnek olarak yönetici paneli engelleniyor
+    },
+    sitemap: 'https://www.nicheprojectagency.com/sitemap.xml',
+  };
+}
